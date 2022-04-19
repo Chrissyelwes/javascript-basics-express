4375f5de9b491cc22e3e8eab023f9f75e33daeeb
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-04-19 21:29:45 +0100

    GET upper and lower test passed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { sayHello } = require('./lib/strings');
+const { uppercase } = require('./lib/strings');
+const { lowercase } = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).json({ result: lowercase(req.params.string) });
 });
 
 module.exports = app;
